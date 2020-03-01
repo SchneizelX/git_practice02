fd3d0c3aca56052e0910f3124b7c186db048ce11
syunaizell <endress_victory@icloud.com>
2020-03-01 14:57:41 +0900

    First commit

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz_convert(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return 'FizzBuzz'
+    elif number % 3 == 0:
+        return 'Fizz'
+    elif number % 5 == 0:
+        return 'Buzz'
+    return number
+
+
+assert fizzbuzz_convert(3) == 'Fizz'
